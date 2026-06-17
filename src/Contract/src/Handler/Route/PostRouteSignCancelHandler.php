fcd84e7964393b95e4e25851b462fa1c83e606b8
Christopher Duarte <christopher.duarte@middletownct.gov>
2026-06-17 10:13:59 -0400

    Fixes #36: Create Custom BoxSign Notification Emails
    Task-Url: https://github.com/comcduarte/ecm-frontend/issues/36

--- src/Contract/src/Handler/Route/PostRouteSignCancelHandler.php
+++ src/Contract/src/Handler/Route/PostRouteSignCancelHandler.php
@@ -14,6 +14,8 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use comcduarte\Box\API\Exception\ClientErrorException;
 use comcduarte\Box\API\Resource\ClientError;
+use comcduarte\Box\API\Resource\File;
+use comcduarte\Box\API\Resource\MetadataInstance;
 use comcduarte\Box\API\Resource\BoxSign\BoxSignRequest;
 class PostRouteSignCancelHandler implements RequestHandlerInterface
 {
@@ -35,20 +37,52 @@ class PostRouteSignCancelHandler implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $access_token = $this->accessTokenService->getAccessToken();
+        $file_id = $request->getAttribute('id');
         
-        $sign_request_id = $request->getAttribute('uuid');
+        /**
+         * Retrieve Metadata from the file
+         * @var \comcduarte\Box\API\Resource\MetadataInstance $metadata
+         */
+        $metadata = new MetadataInstance($access_token);
+        $instances = $metadata->list_metadata_instances_on_file($file_id);
+        $sign_request_id = null;
         
-        $request = new BoxSignRequest($access_token);
-        $result = $request->get_box_sign_request_by_id($sign_request_id);
+        foreach ($instances->entries as $x) {
+            switch ($x['$template']) {
+                case 'boxSign':
+                    $boxsign = new BoxSignRequest($access_token);
+                    $result = $boxsign->get_box_sign_request_by_id($x['signId']);
+                    $sign_request_id = $boxsign->getId();
+                    break;
+                default:
+                    break;
+            }
+        }
         
+        $result = $boxsign->cancel_box_sign_request($sign_request_id);
         if ($result instanceof ClientError) {
+            $this->messenger->addError($result->message);
             throw new ClientErrorException($result->message);
         }
         
-        $result = $request->cancel_box_sign_request($sign_request_id);
-        if ($result instanceof ClientError) {
-            throw new ClientErrorException($result->message);
+        $file = new File($access_token);
+        $sign_files = $boxsign->getSignFiles();
+        foreach ($sign_files['files'] as $sign_file) {
+            $result = $file->delete_file($sign_file['id']);
+            
+            if ($result instanceof ClientError) {
+                throw new ClientErrorException($result->message);
+            }
         }
+        
+        if ($boxsign->getSigningLog() instanceof File) {
+            $result = $file->delete_file($boxsign->getSigningLog()->id);
+            
+            if ($result instanceof ClientError) {
+                throw new ClientErrorException($result->message);
+            }
+        }
+        unset($file);
         
         $uri = $this->router->generateUri('workflow::dashboard', ['action' => 'index']);
         return new RedirectResponse($uri);
